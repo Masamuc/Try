48f1c6b71be900c254dd7d4ae02804e14fabdd84
Mucifer <slibarmasa@gmail.com>
2024-07-27 20:01:47 +0200

    fix6

--- naloga4.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  document.getElementById("MyButton").addEventListener("click", function () {
-    document.getElementById("content").innerText = "You are gay";
-  });
-});
---
+++ randomvaja.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("MyButton").addEventListener("click", function () {
+    document.getElementById("content").innerText = "You are gay";
+  });
+});
